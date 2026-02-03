2c050e2ec4c6be52f3aa5e1a1bd017e882f055cf
Kowit Phetnil <156267717+Kowit1@users.noreply.github.com>
2026-02-03 16:46:34 +0700

    add image

--- lab/app/src/main/java/com/example/a509lablearnandroid/ListActivity.kt
+++ lab/app/src/main/java/com/example/a509lablearnandroid/ListActivity.kt
@@ -4,10 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
@@ -15,6 +18,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.a509lablearnandroid.ui.theme._509LabLearnAndroidTheme
@@ -36,13 +40,27 @@ fun ListScreen() {
         Column(modifier = Modifier.background(Color.Gray).padding(16.dp)) {
             LazyColumn(modifier = Modifier.fillMaxSize().background(Color.White).padding(16.dp)) {
                 items(allKantoPokemon) { item ->
-                    Text(text = item.name)
+                    Row {
+                        Text(text = item.number.toString())
+                        Text(text = item.name)
+                        val imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/${item.number}.png"
+                        AsyncImage(
+                            model = imageUrl,
+                            contentDescription = "Sprite of ${item.name}",
+                            modifier = Modifier.size(64.dp), // Set a consistent size
+                            placeholder = painterResource(id = R.drawable.ic_launcher_foreground), // Optional placeholder
+                            error = painterResource(id = R.drawable.ic_launcher_background) // Optional error image
+                        )
+
+                    }
 
                 }
             }
         }
     }
 }
+
+
 data class Pokemon(
     val name: String,
     val number: Int
