b599b7c5a596f18f1073ccc816c38eb49a276f29
Kowit Phetnil <156267717+Kowit1@users.noreply.github.com>
2026-02-10 15:03:50 +0700

    Add Feature Refector and Bug fixing

--- lab/app/src/main/java/com/example/a509lablearnandroid/PokedexActivity.kt
+++ lab/app/src/main/java/com/example/a509lablearnandroid/PokedexActivity.kt
@@ -1,37 +1,64 @@
 package com.example.a509lablearnandroid
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.a509lablearnandroid.ui.theme._509LabLearnAndroidTheme
 
-class ListActivity : ComponentActivity() {
+class PokedexActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("Lifecycle", "PokedexActivity : onCreate")
         enableEdgeToEdge()
         setContent {
             ListScreen()
 
         }
     }
+    override fun onStart() {
+        super.onStart()
+        Log.i("Lifecycle", "PokedexActivity : onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i("Lifecycle", "PokedexActivity : onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i("Lifecycle", "PokedexActivity : onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i("Lifecycle", "PokedexActivity : onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i("Lifecycle", "PokedexActivity : onDestroy")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("Lifecycle", "PokedexActivity: onRestart")
+}
+
 }
 
 @Composable
@@ -43,14 +70,14 @@ fun ListScreen() {
                     Row {
                         Text(text = item.number.toString())
                         Text(text = item.name)
-                        val imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/${item.number}.png"
-                        AsyncImage(
-                            model = imageUrl,
-                            contentDescription = "Sprite of ${item.name}",
-                            modifier = Modifier.size(64.dp), // Set a consistent size
-                            placeholder = painterResource(id = R.drawable.ic_launcher_foreground), // Optional placeholder
-                            error = painterResource(id = R.drawable.ic_launcher_background) // Optional error image
-                        )
+//                        val imageUrl = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-iii/firered-leafgreen/${item.number}.png"
+//                        AsyncImage(
+//                            model = imageUrl,
+//                            contentDescription = "Sprite of ${item.name}",
+//                            modifier = Modifier.size(64.dp), // Set a consistent size
+//                            placeholder = painterResource(id = R.drawable.ic_launcher_foreground), // Optional placeholder
+//                            error = painterResource(id = R.drawable.ic_launcher_background) // Optional error image
+//                        )
 
                     }
 
